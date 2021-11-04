e9ac73b4c9efeb342a667a173c1075dfa0c28b4e
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2021-11-04 14:42:53 +0100

    refactor: redux hooks.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { connect } from 'react-redux';
+import {  useDispatch, useSelector } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import { auth, handleUserProfile } from './firebase/utils';
 import { setCurrentUser } from './redux/User/user.actions';
@@ -18,21 +18,21 @@ import './default.scss';
 import WithAuth from './hoc/withAuth';
 
 const App = props => {
-  const { currentUser, setCurrentUser } = props;
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const authListener = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await handleUserProfile(userAuth);
         userRef.onSnapshot(snapshot => {
-          setCurrentUser({
+          dispatch(setCurrentUser({
             id: snapshot.id,
             ...snapshot.data()
-          });
+          }));
         })
       }
 
-      setCurrentUser(userAuth);
+      dispatch(setCurrentUser(userAuth));
     });
 
     return () => {
@@ -85,12 +85,4 @@ const App = props => {
   );
 }
 
-const mapStateToProps = ({ user }) =>({
-  currentUser: user.currentUser
-});
-
-const mapDispatchToProps = dispatch =>({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
