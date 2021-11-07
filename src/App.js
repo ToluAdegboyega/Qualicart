385e0eb6638ef903942ae089e8b4c0db9072ad5a
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2021-11-07 21:26:59 +0100

    feat: admin user roles.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import {  useDispatch } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import { checkUserSession } from './redux/User/user.actions';
 
+//components
+import AdminToolbar from './components/AdminToolbar';
+
+//hoc
+import WithAuth from './hoc/withAuth';
+import WithAdminAuth from './hoc/withAdminAuth';
+
+//layouts
 import MainLayout from './layouts/MainLayout';
 import HomepageLayout from './layouts/HomepageLayout';
 
+// pages
 import Homepage from './pages/Homepage';
 import Registration from './pages/Registration';
 import Login from './pages/Login';
 import Recovery from './pages/Recovery';
 import Dashboard from './pages/Dashboard';
+import Admin from './pages/Admin';
 
 import './default.scss';
-
-import WithAuth from './hoc/withAuth';
 
 const App = props => {
   const dispatch = useDispatch();
@@ -25,6 +33,7 @@ const App = props => {
 
   return (
     <div className="App">
+      <AdminToolbar />
       <Switch>
         <Route exact path="/" render={() => (
           <HomepageLayout>
@@ -60,6 +69,15 @@ const App = props => {
                 <Dashboard />
               </MainLayout>
             </WithAuth>
+          )}
+        />
+        <Route path="/admin"
+          render={() => (
+            <WithAdminAuth>
+              <MainLayout>
+                <Admin />
+              </MainLayout>
+            </WithAdminAuth>
           )}
         />
       </Switch>
