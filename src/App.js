1237cc4b54e05ec6b7138422c2d047858339598c
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2021-11-10 15:48:00 +0100

    feat: admin and dashboard layouts.

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import WithAdminAuth from './hoc/withAdminAuth';
 //layouts
 import MainLayout from './layouts/MainLayout';
 import HomepageLayout from './layouts/HomepageLayout';
+import AdminLayout from './layouts/AdminLayout';
+import DashboardLayout from './layouts/DashboardLayout';
 
 // pages
 import Homepage from './pages/Homepage';
@@ -65,18 +67,18 @@ const App = props => {
         <Route path="/dashboard"
           render={() => (
             <WithAuth>
-              <MainLayout>
+              <DashboardLayout>
                 <Dashboard />
-              </MainLayout>
+              </DashboardLayout>
             </WithAuth>
           )}
         />
         <Route path="/admin"
           render={() => (
             <WithAdminAuth>
-              <MainLayout>
+              <AdminLayout>
                 <Admin />
-              </MainLayout>
+              </AdminLayout>
             </WithAdminAuth>
           )}
         />
