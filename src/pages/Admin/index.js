57b852fef55964a7dad1b7017f9c8ac549de2be2
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2021-11-12 21:53:21 +0100

    feat: admin add and manage products.

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { firestore } from './../../firebase/utils';
+import { useDispatch, useSelector } from 'react-redux';
+import { addProductStart, deleteProductStart, fetchProductsStart } from '../../redux/Products/products.actions';
 import Modal from './../../components/Modal';
 import FormInput from './../../components/Forms/FormInput';
 import FormSelect from './../../components/Forms/FormSelect';
@@ -7,14 +8,26 @@ import Button from './../../components/Forms/Button';
 
 import './styles.scss';
 
-const Admin = props => {
+const mapState = ({ productsData }) => ({
+    products: productsData.products
+});
 
-    const [products, setProducts] = useState([]);
+const Admin = props => {
+    const dispatch = useDispatch();
+    const { products } = useSelector(mapState);
+
     const [hideModal, setHideModal] = useState(true);
     const [productCategory, setProductCategory] = useState('mens');
     const [productName, setProductName] = useState('');
     const [productThumbnail, setProductThumbnail] = useState('');
     const [productPrice, setProductPrice] = useState(0);
+
+    useEffect(() => {
+        dispatch(
+            fetchProductsStart()
+        );
+        
+    }, []);
 
     const toggleModal = () => setHideModal(!hideModal);
 
@@ -23,26 +36,27 @@ const Admin = props => {
         toggleModal
     };
 
-    useEffect(() => {
-        firestore.collection('products').get().then(snapshot => {
-        const snapshotData = snapshot.docs.map(doc => doc.data());
-        setProducts(snapshotData);
-        });
-    }, []);
-
+    const resetForm = () => {
+        setHideModal(true);
+        setProductCategory('mens');
+        setProductName('');
+        setProductThumbnail('');
+        setProductPrice(0);
+    }
 
     const handleSubmit = e => {
         e.preventDefault();
 
-        firestore.collection('products').doc().set({
-        productCategory,
-        productName,
-        productThumbnail,
-        productPrice
-        }).then(e => {
-        // Success
-        });
+        dispatch(
+            addProductStart({
+                productCategory,
+                productName,
+                productThumbnail,
+                productPrice
+            })
+        );
 
+        resetForm();
     };
 
     return (
@@ -109,6 +123,55 @@ const Admin = props => {
                     </form>
                 </div>
             </Modal>
+
+            <div className="manageProducts">
+                <table border="0" cellPadding="0" cellSpacing="0">
+                    <tbody>
+                        <tr>
+                            <th>
+                                <h1>
+                                    Manage Products
+                                </h1>
+                            </th>
+                        </tr>
+                        <tr>
+                            <td>
+                                <table className="results"  border="0" cellPadding="10" cellSpacing="0">
+                                    <tbody>
+                                        {products.map((product, index) => {
+                                            const { 
+                                                productName,
+                                                productThumbnail,
+                                                productPrice,
+                                                documentID
+                                            } = product;
+
+                                            return (
+                                                <tr>
+                                                    <td>
+                                                        <img className="thumb" src={productThumbnail} />
+                                                    </td>
+                                                    <td>
+                                                        {productName}
+                                                    </td>
+                                                    <td>
+                                                        ${productPrice}
+                                                    </td>
+                                                    <td>
+                                                        <Button onClick={() =>dispatch(deleteProductStart(documentID))}>
+                                                            Delete
+                                                        </Button>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })}
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
   
         </div>
     )
